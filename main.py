20cbf39d4121f9423ef8d88e403ce4cc069206bc
James Bernardi <james.bernardi@audioanalytic.com>
2020-11-18 23:19:52 +0000

    Adding evaluation and restructuring main file

--- main.py
+++ main.py
@@ -129,8 +129,8 @@ def load_data(
         number=-1,
 ):
 
-    full_xs = np.load(os.path.join(loc, combined_nm))
-    full_ys = np.load(os.path.join(loc, segmented_nm))
+    full_xs = np.load(os.path.join(loc, combined_nm)).astype(np.float32)
+    full_ys = np.load(os.path.join(loc, segmented_nm)).astype(np.int32)
 
     if number > 0:
         xs = full_xs[:number]
@@ -147,9 +147,10 @@ def load_data(
 
 def make_generator(
         train_xs, train_ys, train_split, val_split,
-        train_batch_size=32, val_batch_size=32,
+        train_batch_size, val_batch_size,
 ):
     generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1./255,
         validation_split=(val_split / train_split),
         # Defaults
         featurewise_center=False, samplewise_center=False,
@@ -157,7 +158,7 @@ def make_generator(
         zca_whitening=False, zca_epsilon=1e-06, rotation_range=0, width_shift_range=0.0,
         height_shift_range=0.0, brightness_range=None, shear_range=0.0, zoom_range=0.0,
         channel_shift_range=0.0, fill_mode='nearest', cval=0.0, horizontal_flip=False,
-        vertical_flip=False, rescale=None, preprocessing_function=None,
+        vertical_flip=False, preprocessing_function=None,
         data_format=None, dtype=None
     )
     training_generator = generator.flow(
@@ -201,26 +202,29 @@ def split_data(dset, split=(0.6, 0.2, 0.2), shuffle=True):
     return sets
 
 
-def define_callbacks():
+def define_callbacks(
+        es_delta=0.0001, es_patience=10,
+        rlr_factor=0.33, rlr_patience=5, rlr_delta=0.001, rlr_min=0.001,
+):
 
     early_stop = cbks.EarlyStopping(
-        monitor='val_loss', min_delta=0.0001, patience=10,
-        restore_best_weights=True,
+        monitor='val_loss', min_delta=es_delta, patience=es_patience,
+        restore_best_weights=True, verbose=1, 
         # Defaults
-        verbose=0, mode='auto',
-        baseline=None
+        mode='auto', baseline=None
     )
 
     reduce_plateau = cbks.ReduceLROnPlateau(
         monitor='val_loss', factor=0.33, patience=5,
         min_delta=0.001, min_lr=0.001,
+        verbose=1,
         # Defaults
-        verbose=0, mode='auto',
-        cooldown=0,
+        cooldown=0, mode='auto',
     )
     tensorboard = cbks.TensorBoard(
         log_dir='tb_logs',
         # Defaults
+        # Apparently can't do histograms with generators
         histogram_freq=0, write_graph=True, write_images=False,
         update_freq='epoch', profile_batch=2, embeddings_freq=0,
         embeddings_metadata=None
@@ -230,66 +234,88 @@ def define_callbacks():
 
 
 if __name__ == "__main__":
+    
+    # CONFIGURE
+    weights_file = "trained_weights2.h5"
+    train_batch_size = val_batch_size = 64
+    trn_split, val_split = 0.8, 0.2  # taken out of training only
+    optim = tf.keras.optimizers.Adam(
+        learning_rate=0.0001,
+    )
+    loss = tf.keras.losses.CategoricalCrossentropy(
+         label_smoothing=0.1  # add custom here
+    )
+    callbacks = define_callbacks()
 
     # DATA
     xs, ys = load_data(
+        "/export/home/jamesb/Downloads/kaggle/"
         #   "sample_data", "combined_sample.npy", "segmented_sample.npy"
-        number=200,
+        # number=200,
     )
     
     xs = np.expand_dims(xs, axis=-1).astype(np.float32)
     input_shape = (None, *xs.shape[1:])
     height, width = xs.shape[1:3]
     n_classes = ys.shape[-1]
-    trn_split, val_split = 0.8, 0.2  # taken out of training only
     print("Input data shape", input_shape)
 
     train_xs, test_xs = split_data(xs, (trn_split, 1.-trn_split))
     train_ys, test_ys = split_data(ys, (trn_split, 1.-trn_split))
     del xs, ys
 
-    print("Making dataset generator")
-    # Consider NumpyArrayIterator - actually looks like it's being used behind scenes
-    train_batch_size = val_batch_size = 32
-    training_generator, validation_generator = make_generator(
-        train_xs, train_ys, trn_split, val_split,
-        train_batch_size, val_batch_size,
-    )
-    train_samples = len(train_xs)
-    del train_xs, train_ys
-    
+    if not os.path.exists(weights_file):
+        print("Making dataset generator")
+        training_generator, validation_generator = make_generator(
+            train_xs, train_ys, trn_split, val_split,
+            train_batch_size, val_batch_size,
+        )
+        train_samples = len(train_xs)
+        del train_xs, train_ys
+
     print("Making model")
     model = UNet2D(input_shape[1:], n_classes)
     model.build(input_shape=input_shape)
     model.summary()
     
     # TRAIN
-    model.compile(
-        optimizer="Adam",
-        loss=tf.keras.losses.CategoricalCrossentropy(),
-        metrics=None, loss_weights=None,
-        weighted_metrics=None, run_eagerly=None,
-    )
+    if os.path.exists(weights_file):
+        model.load_weights(weights_file)
+    else:
+        model.compile(
+            optimizer=optim,
+            loss=loss,
+            # Defaults
+            metrics=None, loss_weights=None,
+            weighted_metrics=None, run_eagerly=None,
+        )
 
-    callbacks = define_callbacks()
-    model.fit(
-        training_generator,
-        batch_size=train_batch_size,
-        steps_per_epoch= train_samples * trn_split // train_batch_size,
-        validation_data=validation_generator,
-        validation_batch_size=val_batch_size,
-        validation_steps= train_samples * val_split // val_batch_size,
-        epochs=100,
-        callbacks=callbacks,
-        shuffle=False,  # already done
-        # Defaults
-        class_weight=None,
-        sample_weight=None,
-        initial_epoch=0,
-        validation_freq=1,
-        max_queue_size=10,
-        workers=1,
-        use_multiprocessing=False
-    )
+        model.fit(
+            x=training_generator,
+            validation_data=validation_generator,
+            epochs=500,
+            callbacks=callbacks,
+            shuffle=False,  # already shuffled
+            # Defaults. Ignore steps and batches;
+            # generators handle more cleanly
+            # (and with repeat data)
+            class_weight=None,
+            sample_weight=None,
+            initial_epoch=0,
+            validation_freq=1,
+            max_queue_size=10,
+            workers=1,
+            use_multiprocessing=False
+        )
+        model.save_weights(weights_file)
 
     # TEST
+    test_accuracy = tf.keras.metrics.Accuracy()
+
+    for (x, y) in zip(test_xs[:2], test_ys[:2]):
+        logits = model(np.expand_dims(x, axis=0) / 255)  # rescale
+        prediction = tf.argmax(logits, axis=-1, output_type=tf.int32)
+        flat_y = tf.argmax(np.expand_dims(y, axis=0), axis=-1)
+        test_accuracy(prediction, flat_y)
+
+    print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
