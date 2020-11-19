bfcb688aad89b83b1ebc6a04aa78174c00a46771
j-bernardi <bernardi.james.h@gmail.com>
2020-11-19 13:58:06 +0000

    Fixing UNet upsampling block
    Previously was upsampling rather than using conv2dtranspose,
    and erroneously using transpose layers instead of regular conv2d
    for the concatenated vector processing.
    
    Fixed in line with original unet

--- models/unet_2d.py
+++ models/unet_2d.py
@@ -10,76 +10,94 @@ class UNet2D(tf.keras.Model):
         super(UNet2D, self).__init__()
         w_init = tf.random_normal_initializer()
 
+        ### TODO ###
+        # Rename "encode" as it's whole block, really
+        ############
+
         self.encode_1 = kl.Conv2D(  # encoder at level 1
             filters=16, kernel_size=(2, 2), strides=1,  # (1, 1), 
-            padding='same', activation=None,  # padding default valid
+            padding='same', activation="relu",  # padding default valid
             input_shape=input_shape, data_format="channels_last",
         )
+        # self.relu_1 = kl.ReLU()
         self.batch_norm_1 = kl.BatchNormalization()
-        self.relu_1 = kl.ReLU()
         self.downsample_12 = kl.MaxPooling2D(  # downsample to level 2
             pool_size=(2,2), strides=None, padding="same", data_format=None
         )
 
-        self.encode_2 = kl.Conv2D(32, (2, 2), strides=1, padding="same")
+        self.encode_2 = kl.Conv2D(
+            32, (2, 2), strides=1, padding="same",
+            activation="relu",
+        )
+        # self.relu_2 = kl.ReLU()
         self.batch_norm_2 = kl.BatchNormalization()
-        self.relu_2 = kl.ReLU()
         self.downsample_23 = kl.MaxPooling2D(pool_size=(2,2), padding="same")
 
-        self.level_3 = kl.Conv2D(32, (2, 2), strides=1, padding="same")  # Encode at bottom layer
+        self.level_3 = kl.Conv2D(  # Encode at bottom layer
+            32, (2, 2), strides=1, padding="same",
+            activation="relu",
+        )
+        # self.relu_3 = kl.ReLU()
         self.batch_norm_3 = kl.BatchNormalization()
-        self.relu_3 = kl.ReLU()
 
         # Consider flatten and fully connected here
-        
-        self.upsample_32 = kl.UpSampling2D(  # upsample level 3 to 2
-            size=(2, 2), data_format=None, interpolation='nearest'
+
+        ### TODO ###
+        # Should be up-conv to upsample (and get rid of UpSampling2D),
+        # then process concatenated blocks with 2x convs.
+        # Also, people recommend no batchnorm on 2nd leg,
+        # as it's expensive and you're inputting concat BN
+        # Make sure to activate the convs.
+        ############
+
+        self.upsample_32 = kl.Conv2DTranspose(  # upsample level 3 to 2
+            32, kernel_size=(2, 2), strides=2, 
+            padding='same', activation="relu",
+            data_format=None,
         )
         self.concat_last_2 = kl.Concatenate(axis=-1)
-        self.decode_2 = kl.Conv2DTranspose(
-            16, kernel_size=(2, 2), strides=1, padding="same",
+        self.decode_2 = kl.Conv2D(
+            32, kernel_size=(2, 2), strides=1, 
+            padding="same", activation="relu",
         )
 
-        self.upsample_21 = kl.UpSampling2D(
-            size=(2, 2), interpolation='nearest',
-            data_format="channels_last",
+        self.upsample_21 = kl.Conv2DTranspose(  # upsample level 3 to 2
+            16, kernel_size=(2, 2), strides=2, 
+            padding='same', activation="relu",
+            data_format=None,
         )
         self.concat_last_1 = kl.Concatenate(axis=-1)
-        self.decode_1 = kl.Conv2DTranspose(  # flexible on size
-            16, kernel_size=(2, 2), strides=1, padding="same",
+        self.decode_1 = kl.Conv2D(  # flexible on size
+            16, kernel_size=(2, 2), strides=1,
+            padding="same", activation="relu",
         )
 
         self.out_layer = kl.Conv2D(  # outputs
-            n_classes, kernel_size=1, padding="same"  # TODO or same?
+            n_classes, kernel_size=1, strides=1,
+            padding="same",
         )
 
         # TODO consider fc layer out?
 
     def call(self, x):
         # print("x", x.shape)
-        encoded_1 = self.relu_1(
-            self.batch_norm_1(
-                self.encode_1(x)
-            )
+        encoded_1 = self.batch_norm_1(
+            self.encode_1(x)
         )
         # print("encoded_1", encoded_1.shape)
         level_2_down_input = self.downsample_12(encoded_1)
         # print("l2 down shape", level_2_down_input.shape)
         
-        encoded_2 = self.relu_2(
-            self.batch_norm_2(
-                self.encode_2(level_2_down_input)
-            )
+        encoded_2 = self.batch_norm_2(
+            self.encode_2(level_2_down_input)
         )
         del level_2_down_input
         # print("encoded_2", encoded_2.shape)
         level_3_input = self.downsample_23(encoded_2)
         # print("l3 in", level_3_input.shape)
 
-        level_3_output = self.relu_3(
-            self.batch_norm_3(
-                self.level_3(level_3_input)
-            )
+        level_3_output = self.batch_norm_3(
+            self.level_3(level_3_input)
         )
         del level_3_input
 
@@ -97,6 +115,7 @@ class UNet2D(tf.keras.Model):
         # print("decoded shape 2", decoded_2.shape)
 
         upsampled_from_2 = self.upsample_21(decoded_2)
+        # print("Upsampled from 2", upsampled_from_2.shape)
         del decoded_2
         level_1_up_input = self.concat_last_1(  # or Concatenate(axis=-1)((x, y))
             (upsampled_from_2, encoded_1)
