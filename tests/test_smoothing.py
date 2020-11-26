06dca0be6e0ad297a021d28d78a19cb64ac3a5cc
James Bernardi <Jamie.Bernardii@gmail.com>
2020-11-26 22:26:38 +0000

    Adding test case defs for weighted smooothing
    I have defined the weighted smoothing to be the proportion of
    the self adjacency vs whole row * remainder (to be smoothed)
    
    Tests for smoothing functions are written accordingly

--- tests/test_smoothing.py
+++ tests/test_smoothing.py
@@ -12,6 +12,12 @@ def test_fixed_uniform_smooth():
 
     n_classes = 3
     magnitude = 0.95
+    expected = np.array([
+        [0.95, 0.025, 0.025],
+        [0.025, 0.95, 0.025],
+        [0.025, 0.025, 0.95],
+    ])
+    print(f"Expected\n{expected}")
 
     smooth = fixed_uniform_smoothing(
         n_classes,
@@ -20,13 +26,8 @@ def test_fixed_uniform_smooth():
 
     print(f"Smooth\n{smooth}")
 
-    expected = np.array([
-        [0.95, 0.025, 0.025],
-        [0.025, 0.95, 0.025],
-        [0.025, 0.025, 0.95],
-    ])
-
-    assert np.all(np.isclose(smooth, expected))
+    assert np.all(np.isclose(smooth, expected)),\
+        np.isclose(smooth, expected)
 
 
 def test_fixed_adjacent_smooth():
@@ -44,6 +45,13 @@ def test_fixed_adjacent_smooth():
     # [0, 1, 0],   4, 0, 0
     # [0, 0, 2],   2, 0, 0
     gen = iter([(None, x), ])
+    
+    expected = np.array([
+        [0.94, 0.04,  0.02], 
+        [0.06, 0.94,    0.],
+        [0.06, 0.,    0.94],
+    ])
+    print(f"Expected\n{expected}")
 
     smooth = fixed_adjacent_smoothing(
         n_classes,
@@ -53,31 +61,37 @@ def test_fixed_adjacent_smooth():
     )
     print(f"Smooth\n{smooth}")
 
-    expected = np.array([
-        [0.94, 0.04,  0.02], 
-        [0.06, 0.94,    0.],
-        [0.06, 0.,    0.94],
-    ])
-
-
-    assert np.all(np.isclose(smooth, expected))
+    assert np.all(np.isclose(smooth, expected)),\
+        np.isclose(smooth, expected)
 
 
 def test_weighted_uniform_smooth():
 
     n_classes = 3
-    max_smoothing_magnitude = 0.8
+    max_smoothing_magnitude = 0.90
     x = np.zeros((1, 3, 3, 3), dtype=int)
     x[0, :, :, 0] = 1  # All class 0
-    x[0, 1, 1, 0] = 0  # Switch middle to 1
-    x[0, 1, 1, 1] = 1
-    x[0, 2, 2, 0] = 0  # Switch LwRt to 1
+    x[0, 0, 1, 0] = 0  # Switch top-middle to 1
+    x[0, 0, 1, 1] = 1
+    x[0, 0, 2, 0] = 0  # Switch top-right to 1
+    x[0, 0, 2, 1] = 1
+    x[0, 2, 2, 0] = 0  # Switch low-right to 1
     x[0, 2, 2, 2] = 1
     # label > adj
-    # [0, 0, 0],  12, 4, 2
-    # [0, 1, 0],   4, 0, 0
+    # [0, 1, 1],  12, 3, 2
+    # [0, 0, 0],   3, 2, 0
     # [0, 0, 2],   2, 0, 0
     gen = iter([(None, x), ])
+    # max_mag + (self-adj / (sum of others)) * (1-max_mag)
+
+    adj0 = (1. - max_smoothing_magnitude) * 12./17.
+    r0 = 1. - (max_smoothing_magnitude + adj0)
+    expected = np.array([
+        [0.90+adj0, r0*0.5, r0*0.5], 
+        [0.03,        0.94,   0.03],
+        [0.05,        0.05,   0.90],
+    ])
+    print(f"Expected\n{expected}")
 
     smooth = weighted_uniform_smoothing(
         n_classes,
@@ -87,30 +101,36 @@ def test_weighted_uniform_smooth():
     )
     print(f"Smooth\n{smooth}")
 
-    # TODO - define output
-    expected = np.array([
-        [0.94, 0.04,  0.02], 
-        [0.06, 0.94,    0.],
-        [0.06, 0.,    0.94],
-    ])
-    assert np.all(np.isclose(smooth, expected))
+    assert np.all(np.isclose(smooth, expected)),\
+        np.isclose(smooth, expected)
 
 
 def test_weighted_adjacent_smooth():
 
     n_classes = 3
-    max_smoothing_magnitude = 0.8
+    max_smoothing_magnitude = 0.90
     x = np.zeros((1, 3, 3, 3), dtype=int)
     x[0, :, :, 0] = 1  # All class 0
-    x[0, 1, 1, 0] = 0  # Switch middle to 1
-    x[0, 1, 1, 1] = 1
-    x[0, 2, 2, 0] = 0  # Switch LwRt to 1
+    x[0, 0, 1, 0] = 0  # Switch top-middle to 1
+    x[0, 0, 1, 1] = 1
+    x[0, 0, 2, 0] = 0  # Switch top-left to 1
+    x[0, 0, 2, 1] = 1
+    x[0, 2, 2, 0] = 0  # Switch bottom-right to 1
     x[0, 2, 2, 2] = 1
     # label > adj
-    # [0, 0, 0],  12, 4, 2
-    # [0, 1, 0],   4, 0, 0
+    # [0, 1, 1],  12, 3, 2
+    # [0, 0, 0],   3, 2, 0
     # [0, 0, 2],   2, 0, 0
     gen = iter([(None, x), ])
+    # max_mag + (self-adj / (sum of others)) * (1-max_mag)
+    adj0 =  (1. - max_smoothing_magnitude) * 12./17.
+    r0 = 1. - (max_smoothing_magnitude + adj0)
+    expected = np.array([
+        [0.90+adj0, r0*3./5., r0*2./5.], 
+        [0.06,          0.94,       0.],
+        [0.10,            0.,     0.90],
+    ])
+    print(f"Expected\n{expected}")
 
     smooth = weighted_adjacent_smoothing(
         n_classes,
@@ -120,18 +140,11 @@ def test_weighted_adjacent_smooth():
     )
     print(f"Smooth\n{smooth}")
 
-    # TODO - define output
-    expected = np.array([
-        [0.94, 0.04,  0.02], 
-        [0.06, 0.94,    0.],
-        [0.06, 0.,    0.94],
-    ])
-    assert np.all(np.isclose(smooth, expected))
+    assert np.all(np.isclose(smooth, expected)), np.isclose(smooth, expected)
 
 
 if __name__ == "__main__":
-
     test_fixed_uniform_smooth()
     test_fixed_adjacent_smooth()
-    # test_weighted_uniform_smooth()
-    # test_weighted_adjacent_smooth()
+    test_weighted_uniform_smooth()
+    test_weighted_adjacent_smooth()
